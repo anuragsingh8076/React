76d0cb0088d5bfcb19c9f5b4a8f0d3c50fb4fca7
anuragsingh8076 <anuragrajput9871595773@gmail.com>
2025-07-03 14:34:22 +0530

    Debuging the codes

--- 12MegaBlog/src/store/store.js
+++ 12MegaBlog/src/store/store.js
@@ -1,9 +1,10 @@
 import {configureStore} from '@reduxjs/toolkit';
+import authReducer from '../store/authSlice';
 
 const store = configureStore({
   reducer: {
-
-  }
+       auth: authReducer,
+  },
 })
 
 
